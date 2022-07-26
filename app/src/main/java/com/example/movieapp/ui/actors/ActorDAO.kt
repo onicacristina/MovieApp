a466f4c28532b53678dc7f05b1a2d0004ba1ebe2
onicacristina <onicacristinaelena@yahoo.com>
2022-07-26 10:49:53 +0300

    Adding a local database for saving genre and actor preferences

---
+++ app/src/main/java/com/example/movieapp/ui/actors/ActorDAO.kt
@@ -0,0 +1,32 @@
+package com.example.movieapp.ui.actors
+
+import androidx.room.*
+
+@Dao
+interface ActorDAO {
+
+    @Query("SELECT * FROM actors")
+    fun getAll(): List<Actor>
+
+    @Insert
+    fun save(actor: Actor)
+
+    @Insert
+    fun saveAll(actors: List<Actor>)
+
+    @Delete
+    fun delete(actor: Actor)
+
+    @Delete
+    fun deleteAll(actors: List<Actor>)
+
+    @Query("DELETE FROM actors")
+    fun deleteAll()
+
+    @Transaction
+    fun replaceAll(actors: List<Actor>){
+        deleteAll()
+        saveAll(actors)
+    }
+
+}
